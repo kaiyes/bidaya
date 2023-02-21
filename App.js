16ea59c5ebc023d5f643af24165364b431b67d40
kayes <kaiyes.ansary@gmail.com>
2023-02-21 08:04:52 +0600

    added first chapter

--- App.js
+++ App.js
@@ -1,126 +1,139 @@
-import React, {useState} from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react'
+import type { PropsWithChildren } from 'react'
 import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
+	SafeAreaView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	TouchableOpacity
+} from 'react-native'
 import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
-import {Icon} from '@rneui/themed';
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp
+} from 'react-native-responsive-screen'
+import { Icon } from '@rneui/themed'
 
 const arr = [
-  {
-    image: require('./src/assets/kitab.png'),
-    questions: ['مَا هٰذَا؟ ', 'أيْنَ الْكِتَابُ؟'],
-    ansers: ['هَذا كتابٌ', 'لكتابُ علَى الْمَكتبِ'],
-    test: 'hey',
-  },
-];
+	{
+		image: require('./src/assets/kitab.jpeg'),
+		question: 'مَا هٰذَا؟',
+		answer: 'هَذا كتابٌ'
+	},
+	{
+		image: require('./src/assets/kitab.jpeg'),
+		question: 'أيْنَ الْكِتَابُ؟',
+		answer: 'لكتابُ علَى الْمَكتبِ'
+	},
+	{
+		image: require('./src/assets/kitab.jpeg'),
+		question: 'كِتَابُ مَنْ هذا؟',
+		answer: 'هذا كتابُ مُدَرِّسٍ'
+	},
+	{
+		image: require('./src/assets/kitab.jpeg'),
+		question: 'مَنْ يَدرُسُ هَذا الكِتَابَ؟',
+		answer: 'يدرُسُ حَامِدٌ هَذا الكِتَابَ'
+	},
+	{
+		image: require('./src/assets/kitab.jpeg'),
+		question: 'هَلْ تَدْرُسُ هَذا الكتابَ؟',
+		answer: 'نعم، أَدْرُسُ هذا الكتابَ.'
+	},
+	{
+		image: require('./src/assets/kitab.jpeg'),
+		question: 'لِمَاذا تدرسُ الكتابَ؟',
+		answer: 'أدرُسُ طَلَبًا لِلْعِلْمِ'
+	},
+	{
+		image: require('./src/assets/kitab.jpeg'),
+		question: 'مَتَى تَدْرُسُ الكتابَ؟',
+		answer: 'أدرس صَبَاحًا وَلَيلًا'
+	},
+
+	{
+		image: require('./src/assets/ball.jpeg'),
+		question: 'مَا هٰذِهِ؟',
+		answer: 'هٰذِهِ كُرَةٌ'
+	}
+]
 
 export default function App() {
-  const [qNumber, setQnumber] = useState(0);
-  const [ansNumber, setAnsNumber] = useState(0);
+	const [number, setNumber] = useState(0)
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          flex: 0.9,
-          alignItems: 'center',
-        }}>
-        <Image
-          style={styles.image}
-          resizeMode={'contain'}
-          source={require('./src/assets/kitab.png')}
-        />
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.topView}>
+				<Image
+					style={styles.image}
+					resizeMode={'contain'}
+					source={arr[number].image}
+				/>
+				<TouchableOpacity onPress={() => console.log('s')}>
+					<Text style={styles.question}>{arr[number].question}</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => console.log('s')}>
+					<Text style={styles.question}>{arr[number].answer}</Text>
+				</TouchableOpacity>
+			</View>
 
-        <TouchableOpacity onPress={() => console.log('s')}>
-          <Text style={styles.question}>{arr[0].questions[qNumber]}</Text>
-        </TouchableOpacity>
+			<View style={styles.bottomView}>
+				<TouchableOpacity
+					style={styles.arrow}
+					onPress={() => setNumber(number - 1)}>
+					<Icon
+						name={'arrow-back-circle'}
+						type={'ionicon'}
+						size={30}
+						color={'black'}
+					/>
+				</TouchableOpacity>
 
-        <TouchableOpacity onPress={() => console.log('s')}>
-          <Text style={styles.question}>{arr[0].ansers[ansNumber]}</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View
-        style={{
-          flex: 0.1,
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <TouchableOpacity
-          style={styles.arrow}
-          onPress={() => {
-            setQnumber(qNumber - 1);
-            setAnsNumber(ansNumber - 1);
-          }}>
-          <Icon
-            name={'arrow-back-circle'}
-            type={'ionicon'}
-            size={30}
-            color={'black'}
-          />
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            setQnumber(qNumber + 1);
-            setAnsNumber(ansNumber + 1);
-          }}>
-          <Text style={styles.buttonText}>Continue</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.arrow}
-          onPress={() => {
-            setQnumber(qNumber + 1);
-            setAnsNumber(ansNumber + 1);
-          }}>
-          <Icon
-            name={'arrow-forward-circle'}
-            type={'ionicon'}
-            size={30}
-            color={'black'}
-          />
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => setNumber(number + 1)}>
+					<Text style={styles.buttonText}>Continue</Text>
+				</TouchableOpacity>
+			</View>
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  question: {
-    fontSize: hp('6%'),
-    fontFamily: 'KFGQPCUthmanicScriptHAFS',
-  },
-  image: {
-    width: wp('90%'),
-    height: hp('40%'),
-  },
-  button: {
-    backgroundColor: 'lightgreen',
-    borderRadius: wp('5%'),
-    justifyContent: 'center',
-    fontWeight: 'bold',
-    alignItems: 'center',
-    marginHorizontal: wp('5%'),
-  },
-  buttonText: {
-    fontSize: 22,
-    paddingHorizontal: wp('5%'),
-    paddingVertical: hp('2%'),
-  },
-});
+	container: {
+		flex: 1
+	},
+	question: {
+		fontSize: hp('4%'),
+		fontFamily: 'me_quran',
+		paddingHorizontal: '1%'
+	},
+	image: {
+		width: '90%',
+		height: '40%'
+	},
+	topView: {
+		flex: 0.9,
+		alignItems: 'center'
+	},
+	bottomView: {
+		flex: 0.1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	button: {
+		backgroundColor: 'lightgreen',
+		borderRadius: '5%',
+		justifyContent: 'center',
+		fontWeight: 'bold',
+		alignItems: 'center',
+		marginHorizontal: '5%'
+	},
+	buttonText: {
+		fontSize: 22,
+		paddingHorizontal: '5%',
+		paddingVertical: '2%'
+	}
+})
